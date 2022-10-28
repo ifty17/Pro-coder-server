352ba0a763124e71fc3d9e8743a6d0edc0b70e0b
ifty17 <iftyrahatkabir@gmail.com>
2022-10-28 15:04:01 +0600

    added details

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
+const cors = require('cors');
 const port = process.env.port || 5000;
 
-
+app.use(cors());
 const courses = require('./data/courses.json')
 
 
@@ -11,6 +12,13 @@ app.get('/courses', (req,res) =>{
 })
 
 app.get("/courses/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedCourses = courses.find((course) => course.id === id);
+  res.send(selectedCourses);
+  // console.log(req.params.id);
+});
+
+app.get("/checkout/:id", (req, res) => {
   const id = req.params.id;
   const selectedCourses = courses.find((course) => course.id === id);
   res.send(selectedCourses);
